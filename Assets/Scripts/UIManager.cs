60641111bd8613ee5af7160a462e86f62980f090
BatuhanPC <batuhanluleci@outlook.com>
2024-05-23 15:20:29 +0300

    Relay Server integrated.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,7 +5,7 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour
+public class UIManager : Singleton<UIManager>
 {
     [SerializeField]
     private Button startServerBtn;
@@ -19,8 +19,16 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI playersInGameText;
 
+    [SerializeField]
+    private TMP_InputField joinCodeInput;
+
+    private void Awake()
+    {
+        Cursor.visible = true;
+    }
     private void Update()
     {
+
         playersInGameText.text = $"Players in game: {PlayersManager.Instance.PlayerCount.ToString()}";
     }
 
@@ -38,8 +46,12 @@ public class UIManager : MonoBehaviour
             }
         });
 
-        startClientBtn.onClick.AddListener(() =>
+        startClientBtn.onClick.AddListener(async () =>
         {
+            if (RelayManager.Instance.IsRelayEnabled && !string.IsNullOrEmpty(joinCodeInput.text))
+                await RelayManager.Instance.JoinRelay(joinCodeInput.text);
+
+
             if (NetworkManager.Singleton.StartClient())
             {
                 Logger.Instance.LogInfo("Successfully started the Client");
@@ -51,8 +63,14 @@ public class UIManager : MonoBehaviour
         });
 
 
-        startHostBtn.onClick.AddListener(() =>
+        startHostBtn.onClick.AddListener(async () =>
         {
+            // this allows the UnityMultiplayer and UnityMultiplayerRelay scene to work with and without
+            // relay features - if the Unity transport is found and is relay protocol then we redirect all the 
+            // traffic through the relay, else it just uses a LAN type (UNET) communication.
+            if (RelayManager.Instance.IsRelayEnabled)
+                await RelayManager.Instance.SetupRelay();
+
             if (NetworkManager.Singleton.StartHost())
             {
                 Logger.Instance.LogInfo("Successfully started the Host");
